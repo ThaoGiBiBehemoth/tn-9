15b60f911d78e779fe3a7241a76a00d27d2e3165
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-03-15 17:21:31 +0700

    2nd: add_model_user

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }    #  before_save { self.email = email.downcase }
+  validates :nickname, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
+  validates :email,    presence: true, uniqueness: true, length: { maximum: 250 }, format: { with: URI::MailTo::EMAIL_REGEXP } # Thành
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  def self.digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
+end
