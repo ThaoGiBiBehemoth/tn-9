6d0991f9638345a228fbe4cdf4bb5140686a49e3
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-03-16 15:46:54 +0700

    6th: add_model_task

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save { email.downcase! }    #  before_save { self.email = email.downcase }
   validates :nickname, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
   validates :email,    presence: true, uniqueness: true, length: { maximum: 250 }, format: { with: URI::MailTo::EMAIL_REGEXP } # Thành
-
+  has_many :tasks
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
